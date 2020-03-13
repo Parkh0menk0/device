87368fa74b6e4bd2c1130751c51c69508677adab
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2020-03-13 23:04:52 +0200

    Добавляет модальное окно с интерактивной картой на главной странице проекта "Девайс"

---
+++ source/js/modules/map.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var mapLink = document.querySelector('.feedback__map');
+var mapPopup = document.querySelector('.map');
+var mapClose = mapPopup.querySelector('.map__close');
+
+mapLink.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  mapPopup.style.display = 'block';
+  document.body.style.overflow = 'hidden';
+});
+
+mapClose.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  mapPopup.style.display = 'none';
+  document.body.style.overflow = 'visible';
+});
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    if (mapPopup.style.display === 'block' && document.body.style.overflow === 'hidden') {
+      evt.preventDefault();
+      mapPopup.style.display = 'none';
+      document.body.style.overflow = 'visible';
+    }
+  }
+});
