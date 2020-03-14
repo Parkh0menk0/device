d78f91fdc0f3431b3cb1adb4890dc4539720e2d3
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2020-03-15 01:52:06 +0200

    Добавляет разметку и стили разделу подвала на главной и странице каталога проекта "Девайс"

--- source/js/modules/map.js
+++ source/js/modules/map.js
@@ -1,27 +1,29 @@
 'use strict';
 
-var mapLink = document.querySelector('.feedback__map');
-var mapPopup = document.querySelector('.map');
-var mapClose = mapPopup.querySelector('.map__close');
+(function () {
+  var mapLink = document.querySelector('.feedback__map');
+  var mapPopup = document.querySelector('.map');
+  var mapClose = mapPopup.querySelector('.map__close');
 
-mapLink.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  mapPopup.style.display = 'block';
-  document.body.style.overflow = 'hidden';
-});
+  mapLink.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    mapPopup.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+  });
 
-mapClose.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  mapPopup.style.display = 'none';
-  document.body.style.overflow = 'visible';
-});
+  mapClose.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    mapPopup.style.display = 'none';
+    document.body.style.overflow = 'visible';
+  });
 
-window.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === 27) {
-    if (mapPopup.style.display === 'block' && document.body.style.overflow === 'hidden') {
-      evt.preventDefault();
-      mapPopup.style.display = 'none';
-      document.body.style.overflow = 'visible';
+  window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      if (mapPopup.style.display === 'block' && document.body.style.overflow === 'hidden') {
+        evt.preventDefault();
+        mapPopup.style.display = 'none';
+        document.body.style.overflow = 'visible';
+      }
     }
-  }
-});
+  });
+})();
